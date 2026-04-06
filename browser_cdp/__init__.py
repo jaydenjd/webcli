9999bcff102eff59a40e9b3cee8110f1f6f87ddc
新南 <wujunde.wjd@alibaba-inc.com>
2026-04-06 13:02:49 +0800

    调整readme

--- browser_cdp/__init__.py
+++ browser_cdp/__init__.py
@@ -4,4 +4,4 @@ webcli CDP Tools - Chrome DevTools Protocol tools for webcli skill
 This package provides Python implementations of the CDP Proxy and CLI tools.
 """
 
-__version__ = "2.4.1"
+__version__ = "1.0.1"
